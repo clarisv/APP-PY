411231e42aba8d94372517b48aa15dcbc8a743b2
Clarice Christine <claricesoarescv@gmail.com>
2025-04-16 15:25:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
+    print('3. Alterando estado do restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
@@ -33,7 +33,10 @@ def opcao_invalida():
 
 def exibir_subtitulo(texto):
     os.system('cls')
+    linha = '*' * (len(texto))
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def cadastrar_novo_restaurante():
@@ -49,11 +52,12 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
 
+    print(f'{'nome do restaurante'.ljust(22)} | {'categoria'.ljust(20)} | ''status' )
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
-        ativo = restaurante['ativo']
-        print(f'- {nome_restaurante} | {categoria} | {ativo}')
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo.ljust(20)}')
 
     voltar_ao_menu_principal()
 
